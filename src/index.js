4da453867c1226a77a76da376a93ada9b237a25b
reggiemcdonald <regmcdonald95@gmail.com>
2020-01-25 10:31:21 -0800

    added implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// TODO
+/*
+ * To work with lambdas, we need to define a handler
+ * Handlers are specified in the AWS console
+ * Here I define a few functions to do that
+ */
+
+
+// Given name, return a greeting
+const greeting = (name = "Anonymous") => `Hello, ${name}`;
+
+/*
+ * Construct the response
+ */
+const buildResponse = (name) => {
+    const greet = greeting(name);
+    return {
+        statusCode: 200,
+        body: greet
+    };
+};
+
+/*
+ * Given an event object with key name, return Hello, <name>
+ * @param event = {
+ *     name: <name>
+ * }
+ */
+exports.handler = async (event) => {
+    // Event should have a key name
+    const name = event && event.name;
+    return buildResponse(name);
+};
